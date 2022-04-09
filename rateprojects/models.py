e2d219333eee7a29a55267fbc12b8db3652a84c1
Francisflow <francisngigi948@gmail.com>
2022-04-09 17:32:20 +0300

    Created profile model and made migrations to save changes

--- rateprojects/models.py
+++ rateprojects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_pic=CloudinaryField('image')
+    bio=HTMLField(blank=True, default='Tell us about you and your work')
+    name=models.CharField(blank=True, max_length=40)
+    # contact_info=models.TextField(blank=True, max_length=50)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
