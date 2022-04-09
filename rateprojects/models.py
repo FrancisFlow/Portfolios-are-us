a5b9c3bd04f70a6792bd6c6914fd5673585692ca
Francisflow <francisngigi948@gmail.com>
2022-04-09 23:56:37 +0300

    Added phonenumber to profile model and made migrations

--- rateprojects/models.py
+++ rateprojects/models.py
@@ -2,16 +2,30 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your models here.
 
 class Profile(models.Model):
     profile_pic=CloudinaryField('image')
-    bio=HTMLField(blank=True, default='Tell us about you and your work')
+    bio=models.TextField()  
     name=models.CharField(blank=True, max_length=40)
-    # contact_info=models.TextField(blank=True, max_length=50)
+    phone_number=models.IntegerField(blank=True, default=123456789)
     user=models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
