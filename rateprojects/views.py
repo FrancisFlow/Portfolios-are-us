ba00c1dbf59c4f69ca033b0b00f7817c2cea0e69
Francisflow <francisngigi948@gmail.com>
2022-04-11 11:13:15 +0300

    Created view function to post a new project

--- rateprojects/views.py
+++ rateprojects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import NewUserForm
+from .forms import NewUserForm, UpdateUserProfileForm, NewProjectForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
@@ -20,9 +20,19 @@ def home(request):
 
 @login_required(login_url='/login')
 def new_post(request):
-    # current_user=request.user
-    # if request.method=='POST':
-    pass
+    current_user=request.user
+    if request.method=='POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project=form.save(commit=False)
+            project.user=current_user
+            project.save()
+        return redirect('home')
+    else:
+        form=NewProjectForm()
+    return render(request, 'new_project.html', {'form':form})
+
+
 
 def register_request(request):
     if request.method=='POST':
